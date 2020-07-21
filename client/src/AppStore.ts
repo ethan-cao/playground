0b7a0905503bcfd66dba418608220d0a6b756a03
Guangxue_Cao <guangxue.cao@insightsoftware.com>
2020-07-21 20:47:41 +0200

    update

--- client/src/AppStore.ts
+++ client/src/AppStore.ts
@@ -1,11 +1,12 @@
-import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import { createStore, combineReducers, compose } from "redux";
 import { TodoReducer } from "./Todo/state/TodoReducer";
 
-const composeEnhancers = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose || compose;
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
     combineReducers({
-         todo: TodoReducer
-    }), 
-    composeEnhancers(applyMiddleware(reduxThunk))
+        todo: TodoReducer
+    }),
+    {},
+    composeEnhancers(),
 );
